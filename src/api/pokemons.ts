d9c0bab35876330780ab548549163d9450a07c8b
TutusBOT <barte77pl@gmail.com>
2022-05-06 01:34:26 +0200

    added imports and types, better error handling

--- src/api/pokemons.ts
+++ src/api/pokemons.ts
@@ -1,13 +1,17 @@
+import axios, { AxiosError } from "axios";
+import { ApiList } from "../models/ApiList";
+import { PokemonsListResponse } from "../models/PokemonsListResponse";
 import apiRequest from "../utilities/apiRequest";
 
 export async function getPokemons(offset = 0, limit = 10) {
 	try {
-		const { data } = await apiRequest(
+		const { data } = await apiRequest.get<ApiList<PokemonsListResponse>>(
 			`/pokemon?limit=${limit}&offset=${offset}`
 		);
 
 		return data;
 	} catch (error) {
-		return error;
+		if (axios.isAxiosError(error)) return error.response?.data;
+		return "Failed to fetch data.";
 	}
 }
