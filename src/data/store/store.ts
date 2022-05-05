d38e830e95f6a1c52fff7aeb9307eb4ed2263be9
TutusBOT <barte77pl@gmail.com>
2022-05-06 01:35:38 +0200

    added proper reducer, middleware and type export

--- src/data/store/store.ts
+++ src/data/store/store.ts
@@ -1,7 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "@redux-saga/core";
+import rootSaga from "../../side_effects/rootSaga";
+import { rootReducer } from "../rootReducer";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-	reducer: {},
+	reducer: { rootReducer },
+	middleware: [sagaMiddleware],
 });
+sagaMiddleware.run(rootSaga);
 
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
