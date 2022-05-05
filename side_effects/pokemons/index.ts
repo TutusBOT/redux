fd17df16bfe3eb917edf978b2d645eea03d7673c
TutusBOT <barte77pl@gmail.com>
2022-05-06 01:37:06 +0200

    middleware to get results from pokeapi(redux-saga)

--- side_effects/pokemons/index.ts
+++
@@ -1,9 +0,0 @@
-import { select } from "redux-saga/effects";
-
-function* fetchPokemons() {
-	try {
-		const offset = yield select((store) => {
-			store.pokemons.offset;
-		}); // gets data from the store
-	} catch (error) {}
-}
---
+++ src/side_effects/pokemons/index.ts
@@ -0,0 +1,28 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { call, put, select, takeLatest } from "redux-saga/effects";
+import { getPokemons } from "../../api/pokemons";
+import {
+	getPokemonsListOffsetSelector,
+	pokemonActions,
+} from "../../data/pokemons/pokemonsSlice";
+import { ApiList } from "../../models/ApiList";
+import { PokemonsListResponse } from "../../models/PokemonsListResponse";
+
+function* fetchPokemons(action: PayloadAction<number | undefined>) {
+	try {
+		const offset: ReturnType<typeof getPokemonsListOffsetSelector> =
+			yield select(getPokemonsListOffsetSelector); // gets data from the store
+		const pokemonData: ApiList<PokemonsListResponse> = yield call(
+			getPokemons,
+			offset,
+			action.payload ?? 10
+		);
+		yield put(pokemonActions.fetchSuccess(pokemonData));
+	} catch (error) {
+		yield put(pokemonActions.fetchFailure(error));
+	}
+}
+
+export default function* pokemonsSagas() {
+	yield takeLatest(pokemonActions.fetch.type, fetchPokemons);
+}
