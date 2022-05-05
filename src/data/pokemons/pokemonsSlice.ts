b80f4c6a707c11b7797fe5eed2511e26fe8a141a
TutusBOT <barte77pl@gmail.com>
2022-05-06 01:31:49 +0200

    slice for list of pokemons

--- src/data/pokemons/pokemonsSlice.ts
+++ src/data/pokemons/pokemonsSlice.ts
@@ -1,30 +1,48 @@
-import { createAction, createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ApiList } from "../../models/ApiList";
+import { PokemonsListResponse } from "../../models/PokemonsListResponse";
+import { RootState } from "../store/store";
 
-const initialState = {
-	data: <any>[],
+type PokemonsState = {
+	data: PokemonsListResponse[];
+	offset: number;
+	error: null | string;
+};
+
+const initialState: PokemonsState = {
+	data: [],
 	offset: 0,
 	error: null,
 };
 
-const todoSlice = createSlice({
+const slice = createSlice({
 	name: "pokemons",
 	initialState,
 	reducers: {
-		fetchSuccess: (state, { payload }) => {
+		fetchSuccess: (
+			state,
+			{ payload }: PayloadAction<ApiList<PokemonsListResponse>>
+		) => {
 			const { results } = payload;
 			state.data.push(...results);
-			state.offset += results.lenght;
+			state.offset += results.length;
 		},
-		fetchFailure: (state, { payload }) => {
-			state.error = payload.error;
+		fetchFailure: (state, { payload }: PayloadAction<string>) => {
+			state.error = payload;
 		},
 	},
 });
 
-const pokemonActions = {
-	fetch: createAction("pokemons/fetch"),
-	fetchSuccess: createAction("pokemons/fetchSuccess"),
-	fetchFailure: createAction("pokemons/fetchFailure"),
+export const pokemonActions = {
+	fetch: createAction<number>("pokemons/fetch"),
+	fetchSuccess: createAction<ApiList<PokemonsListResponse>>(
+		"pokemons/fetchSuccess"
+	),
+	fetchFailure: createAction<unknown>("pokemons/fetchFailure"),
 };
 
-export default todoSlice;
+export const getPokemonsListOffsetSelector = (store: RootState) => {
+	store.rootReducer.pokemons.offset;
+};
+
+export default slice.reducer;
